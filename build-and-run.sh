681bec7a1a0d67fc6aeba46523ef5001d9c730cc
Milos Globocki <milos.globocki@student.tugraz.at>
2025-01-13 20:22:38 +0100

    update run scripts

---
+++ build-and-run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+npm run build
+cp dist/* examples/react-example/src/compiled-files-here
+cd examples/react-example/src/
+npm start
+
+#npm start &
+
+#NPM_PID=$!
+
+#sleep 3
+
+#echo "Restarting the application"
+
+#kill $NPM_PID
+
+#sleep 1
+
+#npm start
